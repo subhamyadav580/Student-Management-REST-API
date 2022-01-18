6b6dda763133660f5738792d57af82ce2ca5c178
subhamyadav580 <subhamyadav580@gmail.com>
2022-01-18 17:33:59 +0530

    updating users model

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
